e332a10e11040d5c0f71848bf71c881d7d664a2c
Quân Anh <quancamilee@gmail.com>
2023-03-01 12:48:41 +0700

    Review Route , Update ../routes/index.js

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -32,35 +32,6 @@ router.post(
     requestHandler.validate,
     userController.signup
   );
-// router.post(
-//     "/signup",
-//     body("username")
-//         .exists().withMessage("username is required")
-//         .isLength({ min: 8 }).withMessage("username minimum 8 characters")
-//         .custom(async value => {
-//             const user = await userModel.findOne({ username: value });
-//             if (user) return Promise.reject("username already used");
-//         }),
-
-//     body("password")
-//         .exists().withMessage("password is required")
-//         .isLength({ min: 8 }).withMessage("password minimum 8 characters"),
-
-//     body("confirmPassword")
-//         .exists().withMessage("confirmPassword is required")
-//         .isLength({ min: 8 }).withMessage("confirmPassword minimum 8 characters")
-//         .custom((value, { req }) => {
-//             if (value !== req.body.password) throw new Error("confirmPassword not match");
-//             return true;
-//         }),
-
-//     body("displayName")
-//         .exists().withMessage("displayName is required")
-//         .isLength({ min: 8 }).withMessage("displayName minimum 8 characters"),
-
-//     requestHandler.validate,
-//     userController.signup 
-// );
 
 router.post(
     "/signin",
@@ -125,6 +96,7 @@ router.post(
     body("mediaRate")
         .exists().withMessage("mediaRate is required"),
 
+    requestHandler.validate,
     favoriteController.addFavorite
 );
 
